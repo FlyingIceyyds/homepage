1a3f398d5b977932ded31a25eed754665577db16
FlyingIce000 <2797501375@qq.com>
2025-04-09 17:59:05 +0800

    modified:   README.md 	modified:   index.html 	modified:   static/css/root.css 	modified:   static/css/style.css 	new file:   static/fonts/KrinkesRegularPERSONAL-1.ttf 	modified:   static/img/background.jpg 	new file:   static/img/code/Email.png 	new file:   static/img/code/GitHub.png 	new file:   static/img/code/TG.jpg 	new file:   static/img/code/WeChat.jpg 	new file:   static/img/code/WhatsApp.jpg 	new file:   static/img/code/ZZ.jpg 	modified:   static/img/favicon.ico 	new file:   static/img/logo.jpg 	deleted:    static/img/logo.png 	modified:   static/img/logokuang.png 	deleted:    static/img/qq.jpg 	deleted:    static/img/wxzsm.jpg 	modified:   static/js/script.js 	modified:   static/svg/skillPc.svg 	modified:   static/svg/skillWap.svg 	new file:   vercel.json

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,4 @@
-console.log('%cCopyright © 2024 zyyo.net',
-    'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
-);
-console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
-console.log('%c  ( o.o )', 'color: #8B4513; font-size: 20px;');
-console.log(' %c  > ^ <', 'color: #8B4513; font-size: 20px;');
-console.log('  %c /  ~ \\', 'color: #8B4513; font-size: 20px;');
-console.log('  %c/______\\', 'color: #8B4513; font-size: 20px;');
+console.log('%cowo', 'color: #8B4513; font-size: 20px;');
 
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
@@ -40,11 +33,114 @@ function toggleClass(selector, className) {
     });
 }
 
-function pop(imageURL) {
-    var tcMainElement = document.querySelector(".tc-img");
-    if (imageURL) {
-        tcMainElement.src = imageURL;
-    }
+function popGitHub() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: auto; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <h2 style="margin-bottom: 10px;">GitHub</h2>
+                <p><strong>@FlyingIce000</strong></p>
+                <p><a style="text-decoration: underline;" target="_blank" href="https://github.com/FlyingIce000">github.com/FlyingIce000</a></p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/GitHub.png" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+function popWeChat() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: 300px; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <h2 style="margin-bottom: 10px;">WeChat</h2>
+                <p><strong>User Name：</strong>ちば</p>
+                <p><strong>ID：</strong>@UK_KingX</p>
+                <p></p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/WeChat.jpg" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+function popWhatsApp() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: 300px; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <h2 style="margin-bottom: 10px;">WhatsApp</h2>
+                <p><strong>Name：</strong>FlyingIce</p>
+                <p><strong>Tel:</strong>+44 7707971698</p>
+                <p></p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/WhatsApp.jpg" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+function popTG() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: 300px; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <h2 style="margin-bottom: 10px;">Telegram</h2>
+                <p><strong>User Name：</strong>ちば</p>
+                <p><strong>ID：</strong>@F_QWQ</p>
+                <p></p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/TG.jpg" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+function popEmail() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: 300px; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <h2 style="margin-bottom: 10px;">E-Mail</h2>
+                <p><strong><a style="text-decoration: underline;" target="_blank" href="mailto:ceo@xxidc.top">ceo@xxidc.top</strong></p>
+                <p></p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/Email.png" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+function popZZ() {
+    var tcMainElement = document.querySelector(".tc-main");
+    tcMainElement.innerHTML = `
+        <div class="card" style="width: 300px; border-radius: 15px; display: flex; align-items: center; gap: 15px;">
+            <div class="card-content" style="flex: 1;color: black;">
+                <p><h2>海内存知己，</h2></p>
+                <p><h2>天涯若比邻。</h2></p>
+                <br>
+                <p>使用WeChat赞助</p>
+            </div>
+            <div class="card-image" style="flex-shrink: 0;">
+                <img src="./static/img/code/ZZ.jpg" alt="QR Code" style="width: 130px; height: 130px; border-radius: 10px;">
+            </div>
+        </div>
+    `;
     toggleClass(".tc-main", "active");
     toggleClass(".tc", "active");
 }
@@ -52,12 +148,12 @@ function pop(imageURL) {
 var tc = document.getElementsByClassName('tc');
 var tc_main = document.getElementsByClassName('tc-main');
 tc[0].addEventListener('click', function (event) {
-    pop();
+    // popWX();
+    popGitHub();
 });
 tc_main[0].addEventListener('click', function (event) {
     event.stopPropagation();
 });
-
 
 
 function setCookie(name, value, days) {
@@ -85,35 +181,11 @@ function getCookie(name) {
     return null;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
-
-
-
-
-
 
     var html = document.querySelector('html');
     var themeState = getCookie("themeState") || "Light";
     var tanChiShe = document.getElementById("tanChiShe");
-
-
-
-
-
 
     function changeTheme(theme) {
         tanChiShe.src = "./static/svg/snake-" + theme + ".svg";
@@ -121,12 +193,6 @@ document.addEventListener('DOMContentLoaded', function () {
         setCookie("themeState", theme, 365);
         themeState = theme;
     }
-
-
-
-
-
-
 
     var Checkbox = document.getElementById('myonoffswitch')
     Checkbox.addEventListener('change', function () {
@@ -139,83 +205,56 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
-
     if (themeState == "Dark") {
         Checkbox.checked = false;
     }
 
     changeTheme(themeState);
 
+    // var fpsElement = document.createElement('div');
+    // fpsElement.id = 'fps';
+    // fpsElement.style.zIndex = '10000';
+    // fpsElement.style.position = 'fixed';
+    // fpsElement.style.left = '0';
+    // document.body.insertBefore(fpsElement, document.body.firstChild);
 
+    // var showFPS = (function () {
+    //     var requestAnimationFrame = window.requestAnimationFrame ||
+    //         window.webkitRequestAnimationFrame ||
+    //         window.mozRequestAnimationFrame ||
+    //         window.oRequestAnimationFrame ||
+    //         window.msRequestAnimationFrame ||
+    //         function (callback) {
+    //             window.setTimeout(callback, 1000 / 60);
+    //         };
 
+    //     var fps = 0,
+    //         last = Date.now(),
+    //         offset, step, appendFps;
 
+    //     step = function () {
+    //         offset = Date.now() - last;
+    //         fps += 1;
 
+    //         if (offset >= 1000) {
+    //             last += offset;
+    //             appendFps(fps);
+    //             fps = 0;
+    //         }
 
+    //         requestAnimationFrame(step);
+    //     };
 
+    //     appendFps = function (fpsValue) {
+    //         fpsElement.textContent = 'FPS: ' + fpsValue;
+    //     };
 
-
-
-
-
-
-
-
-
-
-   
-
-    var fpsElement = document.createElement('div');
-    fpsElement.id = 'fps';
-    fpsElement.style.zIndex = '10000';
-    fpsElement.style.position = 'fixed';
-    fpsElement.style.left = '0';
-    document.body.insertBefore(fpsElement, document.body.firstChild);
-
-    var showFPS = (function () {
-        var requestAnimationFrame = window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-
-        var fps = 0,
-            last = Date.now(),
-            offset, step, appendFps;
-
-        step = function () {
-            offset = Date.now() - last;
-            fps += 1;
-
-            if (offset >= 1000) {
-                last += offset;
-                appendFps(fps);
-                fps = 0;
-            }
-
-            requestAnimationFrame(step);
-        };
-
-        appendFps = function (fpsValue) {
-            fpsElement.textContent = 'FPS: ' + fpsValue;
-        };
-
-        step();
-    })();
-    
-    
+    //     step();
+    // })();
     
     //pop('./static/img/tz.jpg')
-    
-    
-    
+
 });
-
-
-
 
 var pageLoading = document.querySelector("#zyyo-loading");
 window.addEventListener('load', function() {
